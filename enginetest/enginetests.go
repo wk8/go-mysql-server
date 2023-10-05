7b102f6aecabe871ae4edd12a65e8c1c4a8f0dc2
jennifersp <jennifer@dolthub.com>
2023-10-05 13:20:34 -0700

    ttt

--- enginetest/enginetests.go
+++ enginetest/enginetests.go
@@ -6051,7 +6051,7 @@ func TestTypesOverWire(t *testing.T, harness ClientHarness, sessionBuilder serve
 			defer engine.Close()
 
 			ctx := NewContextWithClient(harness, sql.Client{
-				User:    "roott",
+				User:    "roottt",
 				Address: "localhost",
 			})
 
